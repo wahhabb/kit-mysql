a7b6860daf23187eaecdfebedb2ea8f8bb729445
WAHHAB BALDWIN <wahhabb@gmail.com>
2023-05-07 21:58:47 -0700

    Merge branch

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -4,6 +4,7 @@ let mysqlconn = null;
 
 export function mysqlconnFn() {
   if (!mysqlconn) {
+    // used for development with MAMP
     // mysqlconn = mysql.createConnection({
     //   host: "127.0.0.1",
     //   user: "root",
