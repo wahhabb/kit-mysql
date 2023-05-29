08e387866b27ec7f0e1ae62e8a7b35c33b93b6c7
WAHHAB BALDWIN <wahhabb@gmail.com>
2023-05-29 10:49:11 -0700

    Add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<header>
+    <h1>
+        State Data from MySQL
+    </h1>
+</header>
+<slot />
+
+<style>
+    header {
+        background-color: aqua;
+        padding: 1.5em 0;
+    }
+    header h1 {
+        text-align: center;
+    }
+
+</style>
