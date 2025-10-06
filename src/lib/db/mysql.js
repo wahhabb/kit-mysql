40f80ee625cb234adbb9052b06e189f8ced71760
Wahhab Baldwin <wahhabb@gmail.com>
2025-10-05 20:23:34 -0700

    Use environment variables to log in

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -1,15 +1,22 @@
 import mysql from "mysql2/promise";
+import { env } from '$env/dynamic/private';
 
+
+console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
 let myPool = null;
 
 export async function myPoolFn() {
   try {
     myPool = await mysql.createPool({
-          host: "srv766.hstgr.io",
-          user: "u384526927_wahhabb",
-          password: "sT4t3s&!ccmc9",
-          database: "u384526927_States",
-    // used for development with MAMP
+          host: env.SQL_HOST,
+          user: env.SQL_USER,
+          password: env.SQL_PASSWORD,
+          database: env.SQL_DATABASE,
+    //       host: "srv766.hstgr.io",
+    //       user: "u384526927_wahhabb",
+    //       password: "sT4t3s&!ccmc9",
+    //       database: "u384526927_States",
+    // // used for development with MAMP
     //   host: "127.0.0.1",
     //   user: "root",
     //   password: "",
