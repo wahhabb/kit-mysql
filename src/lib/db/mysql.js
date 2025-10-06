b3757a0c04833aeb533a2c36909b22a346dfbcd3
Wahhab Baldwin <wahhabb@gmail.com>
2025-10-05 20:27:37 -0700

    Use environment variables to log in

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -12,10 +12,6 @@ export async function myPoolFn() {
           user: env.SQL_USER,
           password: env.SQL_PASSWORD,
           database: env.SQL_DATABASE,
-    //       host: "srv766.hstgr.io",
-    //       user: "u384526927_wahhabb",
-    //       password: "sT4t3s&!ccmc9",
-    //       database: "u384526927_States",
     // // used for development with MAMP
     //   host: "127.0.0.1",
     //   user: "root",
